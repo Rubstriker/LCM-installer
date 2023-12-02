4f6dc484f6f71540d9dd38fba8e2ec47ccc10618
Eliyah Sundström <eliyah@sundstroem.com>
2023-12-02 22:41:13 +0100

    feat: made so plugins are downloaded upon installation

--- .lcm/scripts/package.js
+++ .lcm/scripts/package.js
@@ -1,9 +1,25 @@
 const { exec } = require('child_process');
+const archiver = require('archiver');
 const fss = require('fs');
 const fs = fss.promises;
 const path = require('path');
 const root = path.join(__dirname, '../../');
 
+function zipDirectory(sourceDir, outPath) {
+    const archive = archiver('zip', { zlib: { level: 9 }});
+    const stream = fss.createWriteStream(outPath);
+
+    return new Promise((resolve, reject) => {
+        archive
+            .directory(sourceDir, false)
+            .on('error', err => reject(err))
+            .pipe(stream)
+        ;
+
+        stream.on('close', () => resolve());
+        archive.finalize();
+    });
+}
 function cmd(command, ...args) {
     let cmdPath = JSON.stringify(path.join(root, 'node_modules', command));
     cmdPath += args.map(arg => ` ${arg}`).join('');
@@ -40,12 +56,19 @@ async function package() {
     console.log('Compiling TypeScript...');
     await cmd(path.join('typescript', 'bin', 'tsc'));
 
+    console.log('Packaging modpack...');
+    await zipDirectory(path.join(root, 'pack'), path.join(root, 'lcm-data', 'pack.zip'));
+    await fs.copyFile(path.join(root, 'lcm-data', 'pack.zip'), path.join(root, 'out', 'pack.zip')); // We also want to release the pack.zip file
+
     console.log('Packaging executable...');
     await cmd(path.join('pkg', 'lib-es5', 'bin.js'), JSON.stringify(path.join(root, 'package.json')));
 }
 
 async function clean() {
     console.log('Cleaning up...');
+
+    const pack = `${root}/lcm-data/pack.zip`;
+    await fs.rm(pack, { recursive: true });
 
     const dist = `${root}/dist`;
     await fs.rm(dist, { recursive: true });
