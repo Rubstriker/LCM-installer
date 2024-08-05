afea1ca5e7ccccef09649fa4870d1f785b9b472c
Rubstriker <152400263+Rubstriker@users.noreply.github.com>
2024-08-05 23:17:43 +0200

    Update patch.js

---
+++ pack/patches/mimics/patch.js
@@ -0,0 +1,13 @@
+function patch(data) {
+    const { config } = data;
+    if (!config.camera) config.camera = {
+        monitorResolution: 0,
+        renderDistance: 70,
+        cameraFps: 30
+    };
+
+    let result = "[MONITOR QUALITY]\n";
+    for (const key in config.camera) result += `${key} = ${config.camera[key]}\n`;
+
+    return result;
+}
