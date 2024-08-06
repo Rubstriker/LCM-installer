bff300f2245e4c4cda734415e07f54961e7605f3
Rubstriker <152400263+Rubstriker@users.noreply.github.com>
2024-08-06 23:36:42 +0200

    Added Mirage

--- pack/patches/masked/patch.js
+++ pack/patches/masked/patch.js
@@ -1,10 +1,10 @@
 function patch(data) {
     const { config } = data;
     if (!config.masked) config.masked = {
-        "Use Vanilla Spawns": false
+        "EnableOverrideSpawnChance": true
     };
 
-    let result = "[General]\n";
+    let result = "[MaskedPlayerEnemy]\n";
     for (const key in config.masked) result += `${key} = ${config.masked[key]}\n`;
 
     return result;
